7616e8c4147b77b3c29a408a567360e844bb2846
Patricio Gonzalez <patote.gonzalez@gmail.com>
2022-09-25 18:02:18 -0300

    add and delete task working propperly

---
+++ start/popup/popup.js
@@ -0,0 +1,49 @@
+const tasks = [];
+
+const addTaskBtn = document.querySelector('#add-task-btn');
+const taskContainer = document.querySelector('#task-container');
+
+addTaskBtn.addEventListener('click', () => addTask());
+function renderTask(taskNum) {
+	const taskRow = document.createElement('div');
+
+	const text = document.createElement('input');
+	text.type = 'text';
+	text.placeholder = 'Enter a task...';
+	text.value = tasks[taskNum]
+
+	text.addEventListener('change', () => {
+		tasks[taskNum] = text.value;
+	});
+
+	const deleteBtn = document.createElement('input');
+	deleteBtn.type = 'button';
+	deleteBtn.value = 'X';
+
+	deleteBtn.addEventListener('click', () => {
+		deleteTask(taskNum)
+	});
+
+	taskRow.appendChild(text);
+	taskRow.appendChild(deleteBtn);
+
+	
+	taskContainer.appendChild(taskRow);
+}
+function addTask() {
+	const taskNum = tasks.length;
+	tasks.push('');
+	renderTask(taskNum)
+}
+
+function deleteTask(taskNum) {
+	tasks.splice(taskNum, 1);
+	renderTasks()
+}
+
+function renderTasks() {
+	taskContainer.textContent = ''
+	tasks.forEach((taskText, taskNum) => {
+		renderTask(taskNum)
+	})
+}
